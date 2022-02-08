3bf744c00c38bb895f0b33a2b4b088608ae3b7e1
Mrdynamic-soni <ssoni5930@gmail.com>
2022-02-08 09:13:16 +0530

    fourth commit

--- removing_stop_words.py
+++ removing_stop_words.py
@@ -4,4 +4,23 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-file = 
+file = open("C:\\Users\\Acer\\Desktop\\black_coffer\\data.text","r+")
+# word = file.read().split()
+# print(len(file.read().split()))
+# file_read =file.read()
+
+stop_words = set(stopwords.words('english'))
+
+token_words = word_tokenize(file.read())
+print(len(token_words))
+filtered_words = []
+
+f =open("new_data.txt","a")
+
+for word in token_words:
+    if  not word.lower() in  stop_words:
+        filtered_words.append(word)
+        f.write(f"{word} ")
+print(len(filtered_words))
+
+
